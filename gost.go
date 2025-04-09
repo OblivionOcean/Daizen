6b394a3047e1be3fcabc8c4914576b1b0d0c5bef
Fgaoxing <92655031+Fgaoxing@users.noreply.github.com>
2025-04-09 22:26:26 +0800

    安装包

--- gost.go
+++ gost.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/OblivionOcean/Daizen/cmd"
+import (
+	"github.com/OblivionOcean/Daizen/cmd"
+	_ "github.com/OblivionOcean/Daizen/markdown"
+)
 
 func main() {
 	cmd.Generate()
